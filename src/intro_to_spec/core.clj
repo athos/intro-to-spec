c17f324b1aa9b934db9d5734cda345664be74a9d
OHTA Shogo <athos0220@gmail.com>
2016-06-22 00:26:51 +0900

    Implement basic queue operations

--- src/intro_to_spec/core.clj
+++ src/intro_to_spec/core.clj
@@ -1,6 +1,21 @@
-(ns intro-to-spec.core)
+(ns intro-to-spec.core
+  (:refer-clojure :exclude [empty empty? remove])
+  (:require [clojure.core :as c]
+            [clojure.spec :as s]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn empty [] [nil nil])
+
+(defn add [[f b] x]
+  [f (cons x b)])
+
+(defn empty? [[f b]]
+  (c/empty? f))
+
+(defn front [[f b]]
+  (first f))
+
+(defn remove [[f b]]
+  (if-let [f' (rest f)]
+    [f' b]
+    [(reverse b) nil]))
+
