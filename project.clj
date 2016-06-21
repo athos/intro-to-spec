7287f8546e9b3df04e0266111237e146b1b4a433
OHTA Shogo <athos0220@gmail.com>
2016-06-22 00:27:36 +0900

    Add test.check to deps

--- project.clj
+++ project.clj
@@ -6,4 +6,5 @@
   :dependencies [[org.clojure/clojure "1.9.0-alpha7"]]
   :profiles {:dev {:source-paths ["env"]
                    :dependencies [[org.clojure/tools.namespace "0.3.0-alpha3"]
-                                  [mount "0.1.10"]]}})
+                                  [mount "0.1.10"]
+                                  [org.clojure/test.check "0.9.0"]]}})
