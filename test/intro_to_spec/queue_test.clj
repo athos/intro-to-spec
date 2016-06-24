5338748aed9ccb169ada1320d432c27ae81b9eb2
OHTA Shogo <athos0220@gmail.com>
2016-06-25 00:12:51 +0900

    Add tests for checking algebraic specification for queue

--- test/intro_to_spec/queue_test.clj
+++ test/intro_to_spec/queue_test.clj
@@ -1,7 +1,34 @@
 (ns intro-to-spec.queue-test
-  (:require [clojure.test :refer :all]
-            [intro-to-spec.queue :refer :all]))
+  (:require [clojure.test.check.clojure-test :refer [defspec]]
+            [clojure.test.check.properties :as prop]
+            [clojure.spec :as s]
+            [intro-to-spec.queue :as q]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn retrieve [[f b]]
+  (concat f (reverse b)))
+
+(defn equiv [q q']
+  (= (retrieve q) (retrieve q')))
+
+(defspec prop-empty?
+  (prop/for-all [q (s/gen ::q/queue)]
+    (= (q/empty? q) (equiv q (q/empty)))))
+
+(defspec prop-front-empty
+  (prop/for-all [x (s/gen ::s/any)]
+    (= (q/front (q/add (q/empty) x)) x)))
+
+(defspec prop-front-add
+  (prop/for-all [q (s/gen ::q/non-empty-queue)
+                 x (s/gen ::s/any)]
+    (= (q/front (q/add q x)) (q/front q))))
+
+(defspec prop-remove-empty
+  (prop/for-all [x (s/gen ::s/any)]
+    (equiv (q/remove (q/add (q/empty) x))
+           (q/empty))))
+
+(defspec prop-remove-add
+  (prop/for-all [q (s/gen ::q/non-empty-queue)
+                 x (s/gen ::s/any)]
+    (equiv (q/remove (q/add q x)) (q/add (q/remove q) x))))
