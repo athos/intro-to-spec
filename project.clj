9fe5b54de03d8aa648b45f93104e409d95dbbc37
OHTA Shogo <athos0220@gmail.com>
2016-07-07 11:07:03 +0900

    Bump up Clojure version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha8"]]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha9"]]
   :profiles {:dev {:source-paths ["env"]
                    :dependencies [[org.clojure/tools.namespace "0.3.0-alpha3"]
                                   [mount "0.1.10"]
